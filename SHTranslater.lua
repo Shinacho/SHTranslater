53e46aaf31fccb638e6b5c65b824d65f9a3b5f8e
Shin211 <d0211@live.jp>
2025-08-30 02:28:16 +0900

    Update SHTranslater.lua

--- SHTranslater.lua
+++ SHTranslater.lua
@@ -3,9 +3,7 @@ local NAME = "SHTranslate"
 local function Translate(_, messageType, fromName, text, isFromCS, fromDisplayName)
 
 	if fromDisplayName == GetDisplayName() then return end
-	
---	if NPC_CHANNELS[messageType] then return end
-	
+		
 	if fromDisplayName == "" then return end
 
 
